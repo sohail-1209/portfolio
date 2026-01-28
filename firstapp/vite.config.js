92908d7ac6ac61a28e81287af99028a8ff1286a9
sohail-1209 <sohailpashe@gmail.com>
2026-01-28 09:35:23 +0530

    Add React app build to portfolio

--- firstapp/vite.config.js
+++ firstapp/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/firstapp/', // 👈 MUST match repo or folder name
+  base: '/portfolio/firstapp/',
 })
